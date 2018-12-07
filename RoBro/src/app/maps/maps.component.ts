94640fa5ba4750009e999f0f4403d22aad3ae9c3
Your Name <you@example.com>
2018-12-07 03:47:53 +0100

    Added position to map

--- RoBro/src/app/maps/maps.component.ts
+++ RoBro/src/app/maps/maps.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MeteoService } from '../services/meteo-services.service';
 
 declare let L;
 
@@ -9,16 +10,17 @@ declare let L;
 })
 export class MapsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-    const map = L.map('map').setView([50.6311634, 3.0599573], 12);
-
-
-    L.tileLayer('https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}', {
-      apikey: 'ca195239dacc45c193b6b693fe8513c9',
-      maxZoom: 22
-    }).addTo(map);
+  constructor(private meteoService: MeteoService) {
   }
 
+  ngOnInit() {
+    this.meteoService.getPositionData().then((result: any) => {
+      console.log(result);
+      const map = L.map('map').setView([result.latitude, result.longitude], 12);
+      L.tileLayer('https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}', {
+        apikey: 'ca195239dacc45c193b6b693fe8513c9',
+        maxZoom: 22
+      }).addTo(map);
+    });
+  }
 }
