332306e47655d2ac351bb488306e20867c5ad025
Jordan Hiertz <hiertzjordan@gmail.com>
2018-12-07 06:44:30 +0100

    Add 404

--- RoBro/src/app/app-routing.module.ts
+++ RoBro/src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', loadChildren: './main/main.module#MainModule' },
   { path: 'reminder', loadChildren: './reminder/reminder.module#ReminderModule' },
   { path: 'maps', loadChildren: './maps/maps.module#MapsModule' },
-  { path: 'graph', loadChildren: './graph/graph.module#GraphModule' }
+  { path: 'graph', loadChildren: './graph/graph.module#GraphModule' },
+  { path: 'not-found', loadChildren: './not-found/not-found.module'},
+  { path: '**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
