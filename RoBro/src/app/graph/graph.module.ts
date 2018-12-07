4a3513d3e4f51635ec657d1a8fcc3d063eae86db
Maya Baouche <maya.baouche98@gmail.com>
2018-12-07 04:39:16 +0100

    Ajout du css pour le graphe

--- RoBro/src/app/graph/graph.module.ts
+++ RoBro/src/app/graph/graph.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GraphComponent } from './graph.component';
 import { GraphRoutingModule } from './graph-routing.module';
+import { MatCardModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [GraphComponent],
   imports: [
   CommonModule,
-  GraphRoutingModule
+  GraphRoutingModule,
+  MatCardModule
 ]
 })
 export class GraphModule { }
