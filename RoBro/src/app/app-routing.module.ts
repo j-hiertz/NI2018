aa05385822c34e542d0ccf105944003155270d43
Jordan Hiertz <hiertzjordan@gmail.com>
2018-12-07 03:19:39 +0100

    add leaflet support

--- RoBro/src/app/app-routing.module.ts
+++ RoBro/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'reminder', loadChildren: './reminder/reminder.module#ReminderModule' }
+  { path: 'reminder', loadChildren: './reminder/reminder.module#ReminderModule' },
+  { path: 'maps', loadChildren: './maps/maps.module#MapsModule' }
 ];
 
 @NgModule({
--- RoBro/src/app/app.component.ts
+++ RoBro/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare let L;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
