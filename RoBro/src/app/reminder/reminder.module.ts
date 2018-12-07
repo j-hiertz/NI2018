d711b1835fada996fc666a1a9e6613e2a5674aa1
chouchouneed <chouchouneed@hotmail.fr>
2018-12-07 04:09:50 +0100

    git drop

--- RoBro/src/app/reminder/reminder.module.ts
+++ RoBro/src/app/reminder/reminder.module.ts
@@ -5,13 +5,19 @@ import { ReminderRoutingModule } from './reminder-routing.module';
 import { RecallerComponent } from './recaller/recaller.component';
 import { TodoComponent } from './todo/todo.component';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ReminderComponent, RecallerComponent, TodoComponent],
   imports: [
     CommonModule,
     ReminderRoutingModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class ReminderModule { }
